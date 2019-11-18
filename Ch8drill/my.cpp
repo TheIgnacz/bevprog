f39876a83e449782cfcec2d5aa1c69e6164a598a
TheIgnacz <theignacz@gmail.com>
2019-11-18 23:55:34 +0100

    no message

---
+++ Ch8drill/my.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+#include "my.h"
+#include <iostream>
+
+using namespace std;
+
+int foo;
+
+void print_foo() {
+    cout << foo << '\n';
+}
+
+void print(int i) {
+    cout << i << '\n';
+}
